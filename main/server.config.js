f8b1b74305e84aa1736485dd6c931f7f3a6f39a2
WangPengPT <bbksoftai@gmail.com>
2025-07-24 15:01:12 +0100

    Update server.config.js

--- main/server.config.js
+++ main/server.config.js
@@ -1,15 +1,16 @@
 // ecosystem.config.js
 module.exports = {
   apps: [{
-    name: "",
+    name: "server",
     script: "./src/app.js",
+	interpreter: 'authbind',
+	interpreter_args: '--deep node',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: "800M",
     env: {
       NODE_ENV: "production",
-      PORT: 80,
     }
   }]
 }
