d448f1fb4c7cdaa8e2d8c3c60767702ef6a90d2e
Heng Zheng <hzgge1@iscte-iul.pt>
2025-10-24 15:18:14 +0100

    Update expo.config.js

--- xiaoxiong_configfiles/expo.config.js
+++ xiaoxiong_configfiles/expo.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apps: [{
     name: "XX_EXPO",
     script: "./server.js",
-    cwd: "../dist_xiaoxiong",
+    cwd: "../dist",
     instances: 1,
     autorestart: true,
     watch: false,
@@ -15,9 +15,10 @@ module.exports = {
       PORT: 502,
       QR_ADDR: "https://order.xiaoxiong.pt:502/client.html?table=",
       TABLES_NUMBER: [[2,2],[4,4],[6,6],[8,8],[10,10],[14,14],[16,16],[20,20],[22,22],[24,24],[26,26],[28,28],[30,30],[50,50],[52,52],[60,60],[62,62],[64,64],[70,70],[72,72],[74,74],[80,80],[82,82],[84,84],[90,90],[92,92],[94,94]],
-      SAVE_ADDR: "save/xx_expo",
-      ENABLE_ROAST_DUCK: false,
-	  ENABLE_MENU_LUNCH: false,
+	  ADDR: "https://order.xiaoxiong.pt:502",
+      ADMIN_PASS: "1015",
+	  SAVE_ADDR: "save/xx_expo",
+      JWT_SECRET: "my_jwt_secret_key"
     }
   }]
 }
