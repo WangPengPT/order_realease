22c47d8c0071b12fc6f16a3e48525cd67d41758f
kiwi <kaiweidong2001@gmail.com>
2025-12-03 16:16:17 +0000

    ui fix

--- dist/server.js
+++ dist/server.js
@@ -145,6 +145,10 @@ app.get('/takeReserve', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
+app.get('/reservePage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 main();
 
 async function OnQuit() {
