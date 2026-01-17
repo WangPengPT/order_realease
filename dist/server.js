a0758ef264966232ecb7676753e3a1e30ab0f082
Heng Zheng <hzgge1@iscte-iul.pt>
2026-01-17 17:13:43 +0000

    重定向

--- dist/server.js
+++ dist/server.js
@@ -143,6 +143,10 @@ app.get('/table', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
+app.get('/paymentPage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 app.get('/takeReserve', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
