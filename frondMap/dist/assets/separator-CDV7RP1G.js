3c3d4dc547bc62994c5d26620822960678fd9595
kiwi <kaiweidong2001@gmail.com>
2026-01-30 21:06:40 +0000

    map

---
+++ frondMap/dist/assets/separator-CDV7RP1G.js
@@ -0,0 +1,2 @@
+import{r as e,j as l,d1 as a}from"./vendor-react-C7F4V4j9.js";import{i as m}from"./index-BgP4kwZT.js";const i=e.forwardRef(({className:o,orientation:r="horizontal",decorative:s=!0,...t},p)=>l.jsx(a,{ref:p,decorative:s,orientation:r,className:m("shrink-0 bg-border",r==="horizontal"?"h-[1px] w-full":"h-full w-[1px]",o),...t}));i.displayName=a.displayName;export{i as S};
+//# sourceMappingURL=separator-CDV7RP1G.js.map
