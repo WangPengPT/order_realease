4cc213b9f71ce83e3eafcba016c083f88226beca
kiwi <kaiweidong2001@gmail.com>
2026-02-04 21:13:32 +0000

    map

---
+++ frondMap/dist/assets/paginatedQuery-DAjJ8Cfn.js
@@ -0,0 +1,2 @@
+import{s as h}from"./index-BXUARo7J.js";async function g(f,c,n,e=1e3){let a=[],l=0,r=!0;for(;r;){let s=h.from(f).select(c).range(l*e,(l+1)*e-1);n&&(s=n(s));const{data:t,error:o}=await s;if(o)throw o;t&&t.length>0?(a=a.concat(t),r=t.length===e,l++):r=!1}return a}export{g as f};
+//# sourceMappingURL=paginatedQuery-DAjJ8Cfn.js.map
