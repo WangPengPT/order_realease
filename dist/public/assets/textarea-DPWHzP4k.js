c17b5f8dfb53b7f1926599d467dc69f763f5ee56
Heng Zheng <hzgge1@iscte-iul.pt>
2026-03-14 13:08:09 +0000

    更新管理端

---
+++ dist/public/assets/textarea-DPWHzP4k.js
@@ -0,0 +1,6 @@
+import{f as o,r as t,j as s,l as i}from"./index-D-4SrsBV.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=o("RotateCcw",[["path",{d:"M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8",key:"1357e3"}],["path",{d:"M3 3v5h5",key:"1xhq8a"}]]),n=t.forwardRef(({className:e,...a},r)=>s.jsx("textarea",{className:i("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",e),ref:r,...a}));n.displayName="Textarea";export{c as R,n as T};
