6935ed72a80e93c2ef9f73fe98097f84fe8dd87a
kiwi <kaiweidong2001@gmail.com>
2026-03-17 20:43:12 +0000

    map

---
+++ frondMap/dist/assets/progress-B3ixa72u.js
@@ -0,0 +1,2 @@
+import{r as t,j as a,e7 as r,e8 as i}from"./vendor-react-BEYeT14g.js";import{o as n}from"./index-CDXfrulE.js";const f=t.forwardRef(({className:s,value:e,...l},o)=>a.jsx(r,{ref:o,className:n("relative h-4 w-full overflow-hidden rounded-full bg-secondary",s),...l,children:a.jsx(i,{className:"h-full w-full flex-1 bg-primary transition-all",style:{transform:`translateX(-${100-(e||0)}%)`}})}));f.displayName=r.displayName;export{f as P};
+//# sourceMappingURL=progress-B3ixa72u.js.map
